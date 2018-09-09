cdd46af8385ce74eb2bfd375514377fae0eb851e
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-09 22:28:39 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,56 @@
 # code here!
+
+class School
+
+ROSTER = {}
+
+def initialize(name)
+   
+end
+
+def roster 
+  ROSTER
+end
+
+def add_student(student_name, student_grade)
+  if ROSTER.key?(student_grade)
+    ROSTER[student_grade] << student_name
+  else
+    ROSTER[student_grade] = []
+    ROSTER[student_grade] << student_name
+  end
+end
+
+
+def grade(grade)
+  ROSTER[grade]
+end
+
+
+def sort 
+  ROSTER.sort.to_h
+end
+
+
+
+
+
+
+end
+
+ 
+ 
+#   def initialize(title)
+#     @title = title
+#   end
+ 
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+ 
+#   # create the writer for genre and add the logic for the class constant
+#   def genre=(genre)
+#     @genre = genre
+#     GENRES << genre 
+#   end
+# end
